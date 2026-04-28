daf9bd9ae1c06400e16319d57368f205851dc918
osiexjgong25-cell <osiexjgong25@gmail.com>
2026-04-28 16:01:49 +0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="neo">
+      <h1 className="title">GK DealHunter V3.2</h1>
+
+      <div className="grid">
+        <a href="/batch" className="btn">批量扫描</a>
+        <a href="/clients" className="btn">客户池</a>
+      </div>
+    </div>
+  );
+}
